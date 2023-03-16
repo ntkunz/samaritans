e3b3ace75d7d84ddab80f3917823f30faf417df0
ntkunz <33107555+ntkunz@users.noreply.github.com>
2023-03-16 16:50:48 -0700

    create get categories function, get events axios call, and use effect for on load

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 
 function App() {
+
+  const [ events, setEvents ] = useState([]);
+  const [ categories, setCategories ] = useState([]);
+  
+  // const categories = []; 
+  const locations = []; 
+  
+  //on load get events
+  useEffect(() => {
+      getEvents();
+  }, []);
+  
+  //function to get events array from api then set events
+  function getEvents() {
+      axios.get(`${api}/events`)
+      .then((res) => {
+          console.log(res.data)
+          setEvents(res.data)
+      })
+      //did have res.data before events below but it threw errors
+      .then((events) => {
+        setCategories(getCategories(events))
+        
+      })
+      .catch((err) => {
+          console.log(err);
+      })
+  }
+  
+  function getCategories(array) {
+    let categoriesArray = [];
+    array.forEach(event => {
+      event.category.forEach(cat => {
+      if (!categoriesArray.includes(cat)){
+        categoriesArray.push(cat);
+      }})
+      return categoriesArray;
+    })
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
