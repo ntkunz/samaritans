f20c669314042675f3ab72405564616a2758373a
seyonthomas <seyonthomas01@gmail.com>
2023-03-17 13:57:12 -0700

    The deleted files shouldn't show now

--- client/src/components/EventList/EventList.js
+++ client/src/components/EventList/EventList.js
@@ -8,7 +8,7 @@ export default function EventList({matchingEvents, searchQuery}){
         return(
             <div className="event__outer-wrapper" key={uuid()}>
                 <div className='event__ribbon'>
-                    <p className='event__text event__text--small'>You searched for: <span className='event__text'>{searchQuery} </span> </p>
+                    <p className='event__text event__text--small'>You searched for:  <span className='event__text'>{searchQuery} </span> </p>
                     <p className='event__text event__text--special' >{matchingEvents.length} Results</p>
                 </div>              
                 <div className="event__wrapper">
