60ff0f33de4edb4a36f261b6feba6b971be44458
ntkunz <33107555+ntkunz@users.noreply.github.com>
2023-03-16 19:46:51 -0700

    useStates for events array and categories array and get functions populating those states. Also add button to app.js to console.log state changes. This will be set to a useEffect once we have components rendering this data

--- client/src/App.js
+++ client/src/App.js
@@ -3,64 +3,84 @@ import './App.css';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 
-
+//=========TO GET THE SERVER TO WORK WITH AXIOS, YOU MAY NEED TO ADD 
+//=========APP.USE(CORS()); TO YOUR MIDDLEWARE, AND NPM INSTALL CORS?
 function App() {
 
-  const [ events, setEvents ] = useState([]);
-  const [ categories, setCategories ] = useState([]);
+const api="http://localhost:8080";
+
+  const [ events, setEvents ] = useState(["hello", "goodbye"]);
+  const [ categories, setCategories ] = useState(["cat", "dog"]);
+
+  //======ATTN TEAM, CLICK ON LOG STATES BUTTON TO console.log NEW STATES=====
   
-  // const categories = []; 
-  const locations = []; 
+    // let locations = []; 
+    let categoriesArray = [];
   
   //on load get events
   useEffect(() => {
       getEvents();
   }, []);
-  
+
+
   //function to get events array from api then set events
   function getEvents() {
       axios.get(`${api}/events`)
       .then((res) => {
-          console.log(res.data)
           setEvents(res.data)
+          return res.data
       })
-      //did have res.data before events below but it threw errors
-      .then((events) => {
-        setCategories(getCategories(events))
-        
+      .then((eventsArray) => {
+        const byCategory = getCategories(eventsArray)
+        setCategories(byCategory)
+        console.log('byCategory', byCategory)
+        // console.log("categories", categories)
+        // console.log("events: ", events)
       })
       .catch((err) => {
           console.log(err);
       })
   }
-  
+
+
   function getCategories(array) {
-    let categoriesArray = [];
     array.forEach(event => {
       event.category.forEach(cat => {
-      if (!categoriesArray.includes(cat)){
-        categoriesArray.push(cat);
-      }})
-      return categoriesArray;
+        // changed categories to uppercase to deal with mismatched cases
+        if (!categoriesArray.includes(cat.toUpperCase())) {
+          categoriesArray.push(cat.toUpperCase())
+          console.log('cat array: ', categoriesArray)
+        }
+      })
     })
+    return categoriesArray;
   }
+
+function logStates() {
+    console.log('events', events);
+    console.log('categories: ', categories)
+  }
+
+    //=====THIS WILL BE GET LOCATION FUNCTION SOON=====
+  // function getCategories(array) {
+  //   let categoriesArray = [];
+  //   array.forEach(event => {
+  //     event.category.forEach(cat => {
+  //     if (!categoriesArray.includes(cat)){
+  //       categoriesArray.push(cat);
+  //     }})
+  //     return categoriesArray;
+  //   })
+  // }
 
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <p>{events.length}</p>
+          <p>{events.length}</p>
+          <p>{categories.length}</p>
+          <button onClick={logStates}>log states</button>
       </header>
     </div>
   );
